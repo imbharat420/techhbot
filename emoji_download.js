75b38bdd1554118a78492359eeab99b2e1a07973
imbharat420 <imbharat420@gmail.com>
2022-12-05 00:35:21 +0530

    runOnSave

--- emoji_download.js
+++ emoji_download.js
@@ -1,87 +1,51 @@
-const getEmojis = () => {
+const getEmojis = () =>{
+      // const CONTAINER = document.querySelector(".EMOJI-CONTAINER");
+      const ROWS =  document.querySelectorAll("div[role=rowgroup]");
+      const DATA = [...new Array(ROWS.length)].map((_,i)=>{
+            const elem = ROWS[i]
+            const label = elem.getAttribute("aria-label");
+            const emojis = elem.querySelectorAll("div[role=gridcell]");
+            return [...new Array(emojis.length)].map((_,ii)=>{
+                  const img = emojis[ii].querySelector("img");
+                  return {
+                        label,
+                        alt: img.getAttribute("alt"),
+                        src: img.getAttribute("src")
+                  }
+            })
+      })
 
-      .EMOJI - CONTAINER ");
+      const OBJDATA = {}
 
-      const ROWS = CONTAINER.querySelectorAll(selectors;
-            "div[role=rowgroup]");
+      for(const group of DATA){
+            for(const obj of group){
+                  const {label,src,alt} = obj;
+                  if(OBJDATA[obj.label] && Array.isArray(OBJDATA[label])){
+                        OBJDATA[label].push({src,alt});
+                  }else{
+                        OBJDATA[label] = [{src,alt}]
+                  }
+            }
+      }
+      return OBJDATA; //this return ObjData 
+};
 
-      const DATA = [...new Array(ROWS.Length)].map((_, i: number) = {
+const EMO = getEmojis();
+const EMOTICONS = getEmojis();
 
-                        const CONTAINER = document.querySelector(selectors:
+const getSrc = (arr) =>{
+      return JSON.stringify(arr.map((src)=>src));
+}
+for(const pair of Object.entries(EMO)){
+      console.assert(pair[0]+":");
+      console.log(getSrc(pair[1]))
+}
 
-                                    const elem ROWS[1];
-                                    const label = elem.getAttribute(qualified Name: "aria-label");
+for(const pair of Object.entries(EMOTICONS)){
+      pair[1] = pair[1].map(obj=>{
+            obj["src"] = obj["src"].slice(obj["src"].lastIndexOf("/")+1)
+            return obj;
+      })
+}
 
-                                    const emojis = elem.querySelectorAll(selectors: "div[role-gridcell]");
-
-                                    return [...new Array(emojis.length)].map((_, ii: number) = {
-
-                                                      const img = emojis[ii].querySelector(selectors: "img");
-
-                                                      return {
-
-                                                            Label,
-
-                                                            alt: img.getAttribute(qualifiedName: "alt"),
-
-                                                            src: img.getAttribute(qualifiedName: "src")
-
-                                                      })
-
-                                                const DBJDATA = {};
-
-                                                for (const group of DATA) {
-
-                                                      for (const obj of group) {
-
-                                                            const {
-                                                                  label,
-                                                                  src,
-                                                                  alt) = obj;
-                                                            if (OBJDATA[obj.label] && Array.isArray(OBJDATA[label])) {
-                                                                  OBJDATA[label].push({
-                                                                        src,
-                                                                        alt
-                                                                  });
-
-                                                            } else {
-
-                                                                  OBJDATA[label] = [{
-                                                                        src,
-                                                                        alt
-                                                                  }];
-
-                                                                  return 0B JDATA;
-
-                                                            }
-
-                                                      }
-
-                                                      const getSrc = (arr) = {
-
-                                                                  return JSON.stringify(arr.map(((src
-                                                                  }) = src));
-
-                                                                  const EMO getEmojis();
-
-                                                                  const EMOTICONS = getEmojis();
-
-                                                                  for (const pair of Object.entries(EMO)) {
-
-                                                                        console.log(pair[0] + ": ");
-
-                                                                        console.log(getSrc(pair[1]))
-
-                                                                  }
-
-                                                                  for (const pair of Object.entries(EMOTICONS)) {
-
-                                                                        pair[1] = pair[1].map((obj) => {
-
-                                                                        })
-
-                                                                  }
-
-                                                                  obj["src"] = obj["src"].slice(obj["src"].lastIndexOf(searchElement: "/") + 1)
-
-                                                                  return obj;
+ 
