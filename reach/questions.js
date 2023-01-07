79ff53b27850316f5563717e263561cea9f1ac5d
imbharat420 <imbharat420@gmail.com>
2023-01-07 14:58:24 +0530

    reach folder created

---
+++ reach/questions.js
@@ -0,0 +1,124 @@
+const questions = [
+      `What kind of coding have you been up to lately?`,
+      `Have you been working on any new programming projects?`,
+      `What are you currently learning in your programming studies?`,
+      `Have you been doing any frontend or backend development lately?`,
+      `What kind of programming challenges have you been tackling?`,
+      `Have you been working on any new app or website projects?`,
+      `What are you focusing on in your programming skillset right now?`,
+      `Have you been dabbling in any new programming languages or frameworks?`,
+      `What kind of coding projects have you been working on lately?`,
+      `What have you been doing to expand your programming knowledge?`,
+      `What's new in the world of programming for you?`,
+      `Any exciting coding projects on the horizon?`,
+      `What have you been working on in terms of programming?`,
+      `What kind of programming have you been doing lately?`,
+      `Have you been learning any new programming skills?`,
+      `What programming projects are you currently working on?`,
+      `What are you focusing on in your programming journey right now?`,
+      `Have you been working on any new software projects?`,
+      `What kind of coding have you been tackling lately?`,
+      `Have you been working on any new app development projects?`,
+      `What are you currently learning in your programming studies?`,
+      `Have you been doing any frontend or backend development lately?`,
+      `What kind of programming challenges have you been taking on?`,
+      `Have you been working on any new website projects?`,
+      `What are you focusing on in your programming skillset at the moment?`,
+      `Have you been experimenting with any new programming languages or frameworks?`,
+      `What kind of coding projects have you been tackling recently?`,
+      `What have you been doing to enhance your programming skills?`,
+      `What programming projects have you been working on lately?`,
+      `What are you currently learning in the world of programming?`,
+      `Have you been doing any frontend or backend development recently?`,
+      `What kind of programming challenges have you been working on?`,
+      `Have you been tackling any new app or website projects?`,
+      `What are you focusing on in your programming skillset currently?`,
+      `Have you been exploring any new programming languages or frameworks?`,
+      `What kind of coding projects have you been busy with lately?`,
+      `What have you been doing to improve your programming knowledge?`,
+      `What programming projects are you currently involved in?`,
+      `What are you learning in your programming studies right now?`,
+      //hackathon
+      `Have you been doing any frontend or backend development as of late?`,
+      `Have you attended any hackathons or coding competitions?`,
+      `Have you taken part in any hackathons or coding competitions recently?`,
+      `What hackathons or coding competitions have you participated in?`,
+      `Have you entered any hackathons or coding competitions?`,
+      `How have you been involved in hackathons or coding competitions?`,
+      `Have you participated in any programming challenges or competitions?`,
+      `Have you been to any hackathons or coding competitions lately?`,
+      `What hackathons or coding competitions have you been a part of?`,
+      `Have you signed up for any hackathons or coding competitions recently?`,
+      `How have you been involved in the hackathon or coding competition scene?`,
+      `Have you joined any hackathons or coding competitions recently?`,
+      `What hackathons or coding competitions have you been involved in?`,
+      `Have you participated in any programming marathons or competitions recently?`,
+      `Have you gone to any hackathons or coding competitions lately?`,
+      `What hackathons or coding competitions have you participated in recently?`,
+      `Have you registered for any hackathons or coding competitions recently?`,
+      `How have you been involved in hackathons or coding competitions lately?`,
+      `Have you participated in any hackathons or coding competitions in the past?`,
+      `What hackathons or coding competitions have you taken part in?`,
+      `Have you signed up for any hackathons or coding competitions in the past?`,
+      `How have you been involved in hackathons or coding competitions in the past?`,
+      `Have you participated in any hackathons or coding competitions before?`,
+      `What hackathons or coding competitions have you been a part of in the past?`,
+      `Have you registered for any hackathons or coding competitions in the past?`,
+      `How have you been involved in the hackathon or coding competition scene previously?`,
+      `Have you joined any hackathons or coding competitions in the past?`,
+      `What hackathons or coding competitions have you been involved in previously?`,
+      `Have you participated in any programming marathons or competitions in the past?`,
+      `Have you gone to any hackathons or coding competitions in the past?`,
+      `What hackathons or coding competitions have you participated in previously?`,
+      `What projects have you been working on lately?`,
+      `Have you learned any new programming languages or frameworks recently?`,
+      `What challenges have you faced in your programming work recently?`,
+      `Have you attended any online programming conferences or meetups recently?`,
+      `What advice do you have for someone who is new to programming?`,
+      `Have you contributed to any open source projects recently?`,
+      `What are your favorite programming resources (e.g. blogs, forums, books)?`,
+      `Have you worked on any personal programming projects lately?`,
+      `How do you stay up to date with the latest programming trends and technologies?`,
+      `What do you enjoy most about programming?`,
+      `What programming languages are you most familiar with?`,
+      `Do you prefer working on front-end or back-end development?`,
+      `What do you enjoy most about being a programmer?`,
+      `haha sorry i am saying it randomly but can you motivate me for programming`,
+      `Have you contributed to any open source projects or communities?`,
+      `Have you participated in any hackathons or coding competitions?`,
+]
+      
+
+
+
+const question = ()=>{
+      return questions[Math.floor(Math.random() * questions.length)]
+}
+
+export default question;
+
+/*
+`Hey ${name} 🧑‍💻😅,
+Howzz you?? 
+
+🥲 or howzz this programing fact?? 
+${facts[Math.floor(Math.random() * facts.length)]}
+`,
+`Yo ${name} 😅,
+what you doing on programming nowadays?? 
+`,
+`Yo ${name} 😅,
+
+do you know this programing fact??
+${facts[Math.floor(Math.random() * facts.length)]}
+`,
+`Yo ${name} ,
+can you suggest me some good programming topics?? 🤔
+`,
+`Yo ${name} 😅,
+what's you are upto nowdays?? 🤔`,
+`Hi ${name} , \nHow are you doing?`,
+`Hey ${name}, \n how are you ??  It's been so long `,
+`what's up ${name} `,
+`How are you doing ${name} `,
+*/
--- reach/wish.js
+++ reach/wish.js
@@ -1,15 +1,19 @@
 import fs from "fs";
 import login from "fca-unofficial";
+import question from "./questions.js"
+import greet from "./greet.js"
+import  saveOnFile from "../utils/saveOnFile.js";
+
+
+
 const friends = JSON.parse(fs.readFileSync('./friends.json', 'utf8'));
 const facts = JSON.parse(fs.readFileSync('./facts.json', 'utf8'));
 
-import  saveOnFile from "./utils/saveOnFile.js";
 
 
 let flag = true;
-let msgCount = 0;
 
-login({appState: JSON.parse(fs.readFileSync('./login/techh.json', 'utf8'))}, (err, api) => {
+login({appState: JSON.parse(fs.readFileSync('../login/techh.json', 'utf8'))}, (err, api) => {
     if(err) return console.error(err);
       api.setOptions({listenEvents: true});
       api.listen((err, event) => {
@@ -35,79 +39,54 @@ login({appState: JSON.parse(fs.readFileSync('./login/techh.json', 'utf8'))}, (er
 
 
 
-const customMsg = (name)=>{
-
-let msgs = [
-`Hey ${name} 🧑‍💻😅,
-Howzz you?? 
-
-🥲 or howzz this programing fact?? 
-${facts[Math.floor(Math.random() * facts.length)]}
-`,
-`Yo ${name} 😅,
-what you doing on programming nowadays?? 
-`,
-`Yo ${name} 😅,
-
-do you know this programing fact??
-${facts[Math.floor(Math.random() * facts.length)]}
-`,
-`Yo ${name} ,
-can you suggest me some good programming topics?? 🤔
-`,
-`Yo ${name} 😅,
-what's you are upto nowdays?? 🤔`,
-`Hi ${name} , \nHow are you doing?`,
-`Hey ${name}, \n how are you ??  It's been so long `,
-`what's up ${name} `,
-`How are you doing ${name} `,
-`Hey ${name}\n What projects have you been working on lately?`,
-`Hello ${name}\n Have you learned any new programming languages or frameworks recently?`,
-`Hi ${name}\n What challenges have you faced in your programming work recently?`,
-`How are you ${name}\n Have you attended any online programming conferences or meetups recently?`,
-`Hey ${name}\n What advice do you have for someone who is new to programming?`,
-`Hey ${name}\n Have you contributed to any open source projects recently?`,
-`Hey ${name}\n What are your favorite programming resources (e.g. blogs, forums, books)?`,
-`Hey ${name}\n Have you worked on any personal programming projects lately?`,
-`Hey ${name}\n How do you stay up to date with the latest programming trends and technologies?`,
-`Hey ${name}\n What do you enjoy most about programming?`,
-`Hey ${name}\n What programming languages are you most familiar with?`,
-`Hey ${name}\n Do you prefer working on front-end or back-end development?`,
-`Hey ${name}\n What do you enjoy most about being a programmer?`,
-`Hey ${name}\n haha sorry i am saying it randomly but can you motivate me for programming`,
-`Hey ${name}\n Have you contributed to any open source projects or communities?`,
-`Hey ${name}\n Have you participated in any hackathons or coding competitions?`,
-]
 
 
-let msg =  msgs[Math.floor(Math.random() * msgs.length)]
-      return msg;
+let getRandomDelay = (minDelay,maxDelay)=>{ 
+    minDelay = minDelay*1000 
+    maxDelay = maxDelay*1000
+    return  Math.random() * (maxDelay - minDelay) + minDelay;
 }
 
-console.log(friends[0])
-const minDelay = 30000; // 5 seconds
-const maxDelay = 80000; // 30 seconds
-let getRandomDelay = ()=>  Math.random() * (maxDelay - minDelay) + minDelay;
+function sleep(ms) {
+      return new Promise((resolve) => {
+            setTimeout(resolve, ms);
+      });
+}
 
+let count = 1268
+console.log(friends[count])
 
-let count = 1166
 function sendMessage(api){
       console.log(">> Start sending messages");
-      let inter = setInterval(()=>{
+      let inter = setInterval(async ()=>{
             count++;
             try{
-                  console.log(`>> ${count} : ${friends[count].fullName}  ${friends[count].userID}`,getRandomDelay());
-                  api.sendMessage(customMsg(friends[count].fullName), friends[count].userID) 
+                  console.log(`>> ${count} : ${friends[count].fullName}  ${friends[count].userID}`);
+                  api.sendMessage(greet(friends[count].fullName), friends[count].userID) 
+                  await sleep(getRandomDelay(8,20));
+                  api.sendMessage(question(), friends[count].userID) 
             }catch(err){
                   console.log(err,friends[count].userID);
             }
       
-            if(count == 1200){
+            if(count == 1300){
                   clearInterval(inter)
                   console.log("********************done*******************************")
             }
-      },getRandomDelay())
+      },getRandomDelay(30,70))
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
