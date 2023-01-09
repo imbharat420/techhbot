f60d33f0e4dd497f2a680bccc39aee0ccd4e3bc1
imbharat420 <imbharat420@gmail.com>
2023-01-09 22:40:40 +0530

    reply_back.js

--- fun/reply_back.js
+++ fun/reply_back.js
@@ -3,18 +3,19 @@ import login from 'fca-unofficial';
 
 let flag = true;
 
-login({ appState: JSON.parse(fs.readFileSync('../login/techh.json', 'utf8')) }, (err, api) => {
+login({ appState: JSON.parse(fs.readFileSync('../login/techh.json', 'utf8')) },  (err, api) => {
  if (err) return console.error(err);
  api.setOptions({ listenEvents: true });
- api.listen((err, event) => {
+ api.listen(async (err, event) => {
   if (err) return console.error(err);
   switch (event.type) {
    case 'message':
    case 'message_reply':
     if (event.senderID !== '100054832716212') return;
-    sleep(3000);
+    await sleep(1000);
     api.sendMessage(event.body, event.threadID);
-    api.setMessageReaction(':love:', event.messageID, event.threadID);
+    await sleep(3000);
+    api.setMessageReaction(':love:', event.messageID);
     break;
   }
  });
